c0f0f49c02e910e1ddd9b1b5935d229da13e42ca
ElShroomster <sriram.srivatsan@berkeley.edu>
2024-09-08 15:52:43 -0700

    finish part 1

--- src/passes/global_deadcode_elim.py
+++ src/passes/global_deadcode_elim.py
@@ -5,24 +5,57 @@ import sys
 
 # Meaning focus on purely nicely blocked function
 def remove_dead_code(function):
+    popped = 0
     used = set()
 
     for instruction in function["instrs"]:
         if "args" in instruction:
             for arg in instruction["args"]:
                 used.add(arg)
-    print(used)
-    for instruction in function["instrs"]:
-        print(instruction)
+    i = 0
+    while i < len(function["instrs"]):
+        instruction = function["instrs"][i]
         if "dest" in instruction and instruction["dest"] not in used:
-            print instruction["dest"]
             if "op" not in instruction or instruction["op"] != "call":
-                del instruction
+                function["instrs"].pop(i)
+                popped += 1
+                i -= 1
+        
+        i += 1
+    
+    return popped
+
+def remove_dead_code_no_branches(function):
+    popped = 0
+    used = set()
+
+    i = len(function["instrs"]) - 1
+
+    while i >= 0:
+        instruction = function["instrs"][i]
+        if ("dest" in instruction and instruction["dest"] not in used) \
+        and ("op" not in instruction or instruction["op"] != "call"):
+            function["instrs"].pop(i)
+            popped += 1
+            i -= 1
+            continue
+
+        if "dest" in instruction:
+            used.remove(instruction["dest"])
+        
+        if "args" in instruction:
+            for arg in instruction["args"]:
+                used.add(arg)
+
+        i -= 1
+
+    return popped
 
 if __name__ == "__main__":
     prog = json.load(sys.stdin)
     
     for function in prog["functions"]:
-        remove_dead_code(function)
+        while remove_dead_code(function):
+            pass
     
-    #json.dump(prog, sys.stdout, indent=2)
+    json.dump(prog, sys.stdout, indent=2)
