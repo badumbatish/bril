19086c4383a5e4eeffa0d7a1a69b5c12775403ac
ElShroomster <sriram.srivatsan@berkeley.edu>
2024-10-01 22:36:37 -0700

    help

--- run_all_tests.py
+++ run_all_tests.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import numpy as np
+from matplotlib import pyplot as plt
 
 
 def find_bril_files(directory):
@@ -14,16 +16,19 @@ def find_bril_files(directory):
 
 def run_command_on_files(bril_files, command_template):
     """Run the specified command on each .bril file."""
+    results = []
     for bril_file in bril_files:
-        print(bril_file)
+        result = 0
+        # print(bril_file)
         command = command_template.format(file=bril_file)
         try:
             # print(f"Running command: {command}")
-            subprocess.run(command, shell=True, check=True, timeout=2)
-        except:
-            print(f"Error while running command on {
-                  bril_file}, running for two seconds, stopping it now")
-            continue
+            result = str(subprocess.check_output(command, shell=True, timeout=2))
+        except Exception as e:
+            print(f"Error while running command on {bril_file}, running for two seconds, stopping it now {e}")
+        
+        results.append(result)
+    return results
 
 
 if __name__ == "__main__":
@@ -33,11 +38,19 @@ if __name__ == "__main__":
     liveness_command = "cat {file} | bril2json | ./target/debug/liveness_analysis | bril2txt | wc -l"
     opt_const_prop_command = "cat {file} | bril2json | ./target/debug/optimistic_const_prop | bril2txt | wc -l"
     pes_const_prop_command = "cat {file} | bril2json | ./target/debug/pessimistic_const_prop | bril2txt | wc -l"
+    none_command = "cat {file} | bril2json | bril2txt | wc -l"
 
     bril_files = find_bril_files(directory_to_search)
 
     if bril_files:
         # print(f"Found {len(bril_files)} .bril files.")
-        run_command_on_files(bril_files, opt_const_prop_command)
+        live_res = run_command_on_files(bril_files, liveness_command)
+        opt_res = run_command_on_files(bril_files, opt_const_prop_command)
+        pes_res = run_command_on_files(bril_files, pes_const_prop_command)
+        all_res = run_command_on_files(bril_files, all_command)
+        none_res = run_command_on_files(bril_files, none_command)
     else:
         print("No .bril files found.")
+    
+    for a, b, c, d, e in zip(none_res, live_res, opt_res, pes_res, all_res):
+        print(a, b, c, d, e)
